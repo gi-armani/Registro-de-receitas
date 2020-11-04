72cb2f72e38013021824b5cbc603cea53bfc7d9f
gi-armani <giovana_armani@usp.br>
2020-11-03 23:06:38 -0300

    Alteracao de funcao para conexao com banco

--- server/index.js
+++ server/index.js
@@ -1,17 +1,35 @@
-var pg = require('pg');
-var conString = "postgressql://postgres:felima09@localhost/receitas";
+const pg        = require('pg');
+const express   = require('express');
+const app       = express();
 
-pg.connect(conString, function(err, client, done) {
+const config = {
+    user: 'postgres',
+    database: 'receitas',
+    password: '',
+    port: 5432                  //Default port, change it if needed
+};
 
-  if (err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    done();
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-  });
+// pool takes the object above -config- as parameter
+const pool = new pg.Pool(config);
 
+app.get('/', (req, res, next) => {
+   pool.connect(function (err, client, done) {
+       if (err) {
+           console.log("Can not connect to the DB" + err);
+       }
+       client.query('SELECT * FROM GetAllStudent()', function (err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+       })
+       console.log('foi?');
+   })
+});
+
+app.listen(4000, function () {
+    //console.log('Server is running on port 4000');
+    console.log('eita');
 });
